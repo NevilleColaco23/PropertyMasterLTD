4a810c7f5213b5e74f10ec60d47ee705997cb25c
neville <NEVILLE@23>
2025-06-22 23:15:41 +0300

    login UI page is added...no functionlity or call to backend

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,11 +1,16 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { LoginFormComponent } from './core/login-form/login-form.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,8 +18,16 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     LoginFormComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
-    AppRoutingModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    HttpClientModule,
   ],
   providers: [
     provideAnimationsAsync()
