135d54448a8eeddeabfeb99c849517e677f93695
neville <NEVILLE@23>
2025-06-28 03:35:09 +0300

    just added test dashboard ui random image generatoe

--- app/src/app/core/dashboards/dashboard-default/dashboard-default.component.ts
+++ app/src/app/core/dashboards/dashboard-default/dashboard-default.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-default',
   templateUrl: './dashboard-default.component.html',
   styleUrl: './dashboard-default.component.css'
 })
-export class DashboardDefaultComponent {
+export class DashboardDefaultComponent implements OnInit {
+  imageUrl: string = '';
 
-  constructor() { 
-    // Initialization logic can go here if needed
-    console.log('DashboardDefaultComponent initialized');
+  private imageUrls: string[] = [
+    'https://picsum.photos/seed/pic1/600/400',
+    'https://picsum.photos/seed/pic2/600/400',
+    'https://picsum.photos/seed/pic3/600/400',
+    'https://picsum.photos/seed/pic4/600/400',
+    'https://picsum.photos/seed/pic5/600/400'
+  ];
+
+  ngOnInit(): void {
+    this.showRandomImage();
+  }
+
+  showRandomImage() {
+    const randomIndex = Math.floor(Math.random() * this.imageUrls.length);
+    this.imageUrl = this.imageUrls[randomIndex];
   }
 }
