a886f97690ff34acca5cd8f0f4b26836e69b643c
neville <NEVILLE@23>
2026-02-28 02:30:32 +0300

    Enable full logging in Production for Railway debugging

--- WebApi/Logging/LoggingStartup.cs
+++ WebApi/Logging/LoggingStartup.cs
@@ -27,9 +27,8 @@ internal static class LoggingStartup
             }
             else
             {
-                // TEMPORARILY SHOW ALL LOGS TO DEBUG RAILWAY DEPLOYMENT
+                // TEMPORARY: Show all logs in Production for debugging Railway deployment
                 loggerCfg
-                .MinimumLevel.Information()
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
                 .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
                 .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information);
